503915453292ac770357fdeac71e540911e52bb5
Donald M Shea <don.shea@nasa.gov>
2019-03-26 10:30:27 -0400

    first try at a bilge pump class

---
+++ pi/bilge_pump.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from gpiozero import Button, LED
+from signal import pause
+from time import time, ctime
+
+bilgePump1Pin = 13
+bilgePump2Pin = 19
+led1Pin = 18
+
+class BilgePump(Button):
+    """A class to track the bilge pump cycling"""
+
+    def __init__(self, name, pin, alert=False):
+        super(). __init__(pin)
+        self.name = name
+        self.alert = alert
+        self.onTime = time()
+        self.when_pressed = self.pumpOn
+        self.when_released = self.pumpOff
+        
+    def pumpOn(self):
+        print("pump", self.pin.number, " ON")
+        if alert:
+            print("**** Send a text message to Don since this is pump", self.pin.number)
+        self.onTime = time()
+
+    def pumpOff(self):
+        print("pump", self.pin.number, " OFF")
+        offTime = time()
+        delta = offTime - self.onTime
+        print(ctime(offTime), "- pump", self.pin.number, ", duration =", delta)
+    
+
+
+
+bilgePump1 = BilgePump("Main (small)", bilgePump1Pin)
+bilgePump2 = BilgePump("Backup (big)", bilgePump2Pin)
+led1 = LED(led1Pin)
+
+# led1.source = bilgePump1
+
+
+
+pause()
+
